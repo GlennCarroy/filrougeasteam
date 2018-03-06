193205f20ad5b5f199c8a4a35617bf94686fdb23
GlennCarroy <g_carroy@live.be>
2018-03-06 12:19:08 +0100

    admin commencée. On peut créer des postes / créer et supprimer des catégories

---
+++ DBA/blog/admin_blog_header.php
@@ -0,0 +1,10 @@
+<header>
+      <nav>
+        <ul>
+          <li><a href="admin_dashboard.php">Dashboard</a></li>
+          <li><a href="admin_create_post.php">Créer un post</a></li>
+          <li><a href="admin_create_categories.php">Ajouter une catégorie</a></li>
+          <li><a href="admin_logout.php">Me déconnecter</a></li>
+        </ul>
+      </nav>
+</header>
