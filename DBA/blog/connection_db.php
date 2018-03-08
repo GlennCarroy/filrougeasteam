da7d75582ecb402eee9c8a55667dbfc2c38e0dc0
Marine <marine@MacBook-Air-de-Marine.local>
2018-03-08 15:43:15 +0100

    heroku connection

--- DBA/blog/connection_db.php
+++ DBA/blog/connection_db.php
@@ -1,16 +1,23 @@
 <?php
+$host='ec2-54-75-244-248.eu-west-1.compute.amazonaws.com';
+$port=5432;
+$dbname='df7ec37j1tssli';
+$user='jxkvacjrsdhepi';
+$pwd='8e98d59ce72f3d7156205f046a94de8f8daabdbf916e1171a35ea151ee52c1d2';
 
-try {
-    $strConnection = 'mysql:host=localhost;dbname=filrougeasteam'; //Ligne 1
-    $arrExtraParam= array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"); 
-    $pdo = new PDO($strConnection, 'root', '', $arrExtraParam); // Instancie la connexion
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Ligne 4
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'filrougeasteam');
+ 
+/* Tentative de connexion à la base de données MySQL */
+try{
+    $pdo = new PDO('pgsql:host='.$host.';port='.$port.';dbname='.$dbname.';user='.$user.';password='.$pwd.'');
+    // Vérifier la connexion
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){
+    die("ERROR: Could not connect. " . $e->getMessage());
 }
-catch(PDOException $e) {
-    $msg = 'ERREUR PDO dans ' . $e->getFile() . ' L.' . $e->getLine() . ' : ' . $e->getMessage();
-    die($msg);
-}
-
-?>	
+?>
 
 
--- DBA/blog/login/config.php
+++ DBA/blog/login/config.php
@@ -1,4 +1,10 @@
 <?php
+$host='ec2-54-75-244-248.eu-west-1.compute.amazonaws.com';
+$port=5432;
+$dbname='df7ec37j1tssli';
+$user='jxkvacjrsdhepi';
+$pwd='8e98d59ce72f3d7156205f046a94de8f8daabdbf916e1171a35ea151ee52c1d2';
+
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
@@ -6,7 +12,7 @@ define('DB_NAME', 'filrougeasteam');
  
 /* Tentative de connexion à la base de données MySQL */
 try{
-    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    $pdo = new PDO('pgsql:host='.$host.';port='.$port.';dbname='.$dbname.';user='.$user.';password='.$pwd.'');
     // Vérifier la connexion
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e){
